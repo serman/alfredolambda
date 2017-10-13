34e2eade77e55c8f74263a9859352bef34081f48
Sergio <sergio@tinkerista.com>
2017-10-13 12:24:59 +0200

    flip cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 
 window.onload = function() {
-    
+    $('.card-container').on('click',function(){$(this).toggleClass('hover') })
 
+    $('#showmore').on('click',function(event){
+        event.preventDefault();
+    })
+    setCardSizes();
+}
+
+$( window ).resize(function() {
+    setCardSizes();
+});
+
+function setCardSizes(){
+    $('.card .front, .card .back').each(function(){
+        console.log($(this))
+        var h= $(this).children('.image-prj').css('height')
+        $(this).css('height',h );
+        $(this).parent().css('height',h );
+    })
 }
